de8b2a2317e916dce5743d089c1b6bdb46966e39
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-11-02 16:58:53 +0100

    add ts support

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-sass`,
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Image from '../components/image'
 const IndexPage = () => (
 	<Layout>
 		<h1>Hi people</h1>
-		<p>Welcome to your new Gatsby site.</p>
+		<p>Welcome to your new Staticfy site.</p>
 		<p>Now go build something great.</p>
 		<div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
 			<Image />
