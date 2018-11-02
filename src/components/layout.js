270ff951474886098d1d39a6db767bfeef41c78b
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-11-02 16:40:28 +0100

    modify prettier

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,45 +7,45 @@ import Header from './header'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+	<StaticQuery
+		query={graphql`
+			query SiteTitleQuery {
+				site {
+					siteMetadata {
+						title
+					}
+				}
+			}
+		`}
+		render={data => (
+			<>
+				<Helmet
+					title={data.site.siteMetadata.title}
+					meta={[
+						{ name: 'description', content: 'Sample' },
+						{ name: 'keywords', content: 'sample, something' },
+					]}
+				>
+					<html lang="en" />
+				</Helmet>
+				<Header siteTitle={data.site.siteMetadata.title} />
+				<div
+					style={{
+						margin: '0 auto',
+						maxWidth: 960,
+						padding: '0px 1.0875rem 1.45rem',
+						paddingTop: 0,
+					}}
+				>
+					{children}
+				</div>
+			</>
+		)}
+	/>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,11 +4,11 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const SecondPage = () => (
-  <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+	<Layout>
+		<h1>Hi from the second page</h1>
+		<p>Welcome to page 2</p>
+		<Link to="/">Go back to the homepage</Link>
+	</Layout>
 )
 
 export default SecondPage
