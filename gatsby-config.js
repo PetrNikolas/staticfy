d12b45b178b469ca9f0362fa9879d6bcb642da0b
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-03-03 12:23:29 +0100

    assets folder for images, etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     {
@@ -48,7 +48,7 @@ module.exports = {
         background_color: '#24EAA9',
         theme_color: '#24EAA9',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/assets/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     {
